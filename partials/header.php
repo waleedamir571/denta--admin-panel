7d390c4ae41cd1753afdf1e0238d5cae4b700900
waleedamir571 <waleedamir063@gmail.com>
2025-05-02 01:56:38 +0500

    changes

--- partials/header.php
+++ partials/header.php
@@ -377,6 +377,20 @@
                                 <span>All Trainers</span>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="all-students.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <path d="M3 16.5L12 21.75L21 16.5" stroke="#8C94A3" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3 12L12 17.25L21 12" stroke="#8C94A3" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3 7.5L12 12.75L21 7.5L12 2.25L3 7.5Z" stroke="#8C94A3" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                                <span>All Students</span>
+                            </a>
+                        </li>
                         
                         <li class="nav-item">
                             <a class="nav-link" href="settings.php">
